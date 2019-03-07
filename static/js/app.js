8248b176cb9e10bb3dbdcb4696896432c554a9b4
JayMcDermott88 <jay.mcdermott93@gmail.com>
2019-03-06 22:15:35 -0600

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,14 @@
-// from data.js
 var tableData = data;
+
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+  
