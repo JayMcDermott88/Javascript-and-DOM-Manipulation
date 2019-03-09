74f6179918ca0958dfbfd687424f5cd60d62addb
JayMcDermott88 <jay.mcdermott93@gmail.com>
2019-03-08 21:53:34 -0600

    update

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,34 @@ data.forEach((sighting) => {
     });
   });
   
+
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", function() {
+
+  
+  d3.event.preventDefault();
+
+ 
+  var inputElement = d3.select("#datetime");
+
+  
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+});
+ 
+// d3.select(".table table-striped")
+//     .append("table-head").text(${Date})
+//     .append("table-head").text(${City})
+//     .append("table-head").text(${State})
+//     .append("table-head").text(${Country})
+//     .append("table-head").text(${Shape})
+//     .append("table-head").text(${Duration})
+//     .append("table-head").text(${Comments})
+// });
